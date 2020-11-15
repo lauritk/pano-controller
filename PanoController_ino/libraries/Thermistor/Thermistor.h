e4438fb36d23a6e2ffc49d6721cfd3579b650cd0
Lauri Kantola <contact@laurikantola.info>
2020-11-15 20:48:26 +0200

    Fix function default value warning

--- PanoController_ino/libraries/Thermistor/Thermistor.h
+++ PanoController_ino/libraries/Thermistor/Thermistor.h
@@ -8,7 +8,7 @@ class Thermistor
   public:
     Thermistor(byte pin, unsigned long nominalRes, int nominalTemp,
       unsigned int bCoeff, unsigned long seriesRes);
-    float read(int numSamples=1);
+    float read(int numSamples);
   private:
     byte pin;
     unsigned long nominalRes;
